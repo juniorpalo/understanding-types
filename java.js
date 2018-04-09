cde41a9cbb0c571ea2814dc47ea06431ab7f478f
Juan Palomino <juniorpalo16@gmail.com>
2018-04-08 21:21:50 -0600

    first commit

---
+++ java.js
@@ -0,0 +1,14 @@
+let first = 'Juan';
+let last = ' Palomino';
+let name = first + last;
+
+console.log(name)
+
+let a = 23;
+let b = 35;
+let calculation = a + b;
+console.log(calculation)
+
+let unexpected = calculation + name;
+console.log(unexpected)
+ 
